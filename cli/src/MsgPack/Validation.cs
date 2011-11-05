043ce496c63712353f391c912239fc2a0d309c31
yfakariya <yfakariya@gmail.com>
2011-11-05 23:46:52 +0900

    Code clean up

--- cli/src/MsgPack/Validation.cs
+++ cli/src/MsgPack/Validation.cs
@@ -72,15 +72,6 @@ namespace MsgPack
 			}
 		}
 
-		//public static void ValidateIsNotNull<T>( T value, string parameterName )
-		//    where T : class
-		//{
-		//    if ( value == null )
-		//    {
-		//        throw new ArgumentNullException( parameterName );
-		//    }
-		//}
-
 		private static void ValidateIsNotNullNorEmpty( string value, string parameterName )
 		{
 			if ( value == null )
