d77f734e2bf1b3e5105328336c7cc65bb58bf486
yfakariya <yfakariya@gmail.com>
2011-11-27 03:28:15 +0900

    Add verification script

---
+++ cli/test/MsgPack.UnitTest/PEVerify.ps1
@@ -0,0 +1,10 @@
+foreach( $file in dir MsgPack.Serialization.GeneratedSerealizers*.dll )
+{
+	$result = ( PEVerify.exe $file /quiet )
+	Write-Host $result
+	if( !$result.EndsWith( "ƒpƒX" ) )
+	{
+		$result = ( PEVerify.exe $file /verbose )
+		Write-Warn $result
+	}
+}
