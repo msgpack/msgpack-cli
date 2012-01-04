c700766d919e942936065f546a60dc7b12b88a21
yfakariya <yfakariya@gmail.com>
2012-01-04 19:56:39 +0900

    Add exception test cases.

---
+++ cli/test/MsgPack.UnitTest/GenericExceptionTester.cs
@@ -0,0 +1,178 @@
+﻿#region -- License Terms --
+//
+// MessagePack for CLI
+//
+// Copyright (C) 2010 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Runtime.Serialization.Formatters.Binary;
+using NUnit.Framework;
+
+namespace MsgPack
+{
+	/// <summary>
+	///		Test suite for standard exception constructors, properties, and serialization.
+	/// </summary>
+	/// <typeparam name="T">Target exception type.</typeparam>
+	public sealed class GenericExceptionTester<T>
+		where T : Exception
+	{
+		private static readonly Type[] _messageConstructorParameterTypes = new[] { typeof( string ) };
+		private static readonly Type[] _innerExceptionConstructorParameterTypes = new[] { typeof( string ), typeof( Exception ) };
+
+		private readonly Func<T> _defaultConstructor;
+		private readonly Func<string, T> _messageConstructor;
+		private readonly Func<string, Exception, T> _innerExceptionConstructor;
+		private readonly Exception _constructorException;
+
+		/// <summary>
+		///		Initializes a new instance of the <see cref="GenericExceptionTester&lt;T&gt;"/> class.
+		/// </summary>
+		public GenericExceptionTester()
+		{
+			try
+			{
+				this._defaultConstructor = Expression.Lambda<Func<T>>( Expression.New( typeof( T ).GetConstructor( Type.EmptyTypes ) ) ).Compile();
+				var message = Expression.Parameter( typeof( string ), "message" );
+				var innerException = Expression.Parameter( typeof( Exception ), "innerException" );
+				this._messageConstructor =
+					Expression.Lambda<Func<string, T>>(
+						Expression.New( typeof( T ).GetConstructor( _messageConstructorParameterTypes ), message ),
+						message
+					).Compile();
+				this._innerExceptionConstructor =
+					Expression.Lambda<Func<string, Exception, T>>(
+						Expression.New( typeof( T ).GetConstructor( _innerExceptionConstructorParameterTypes ), message, innerException ),
+						message,
+						innerException
+					).Compile();
+			}
+			catch ( Exception ex )
+			{
+				this._constructorException = ex;
+			}
+		}
+
+		/// <summary>
+		///		Tests the exception.
+		/// </summary>
+		public void TestException()
+		{
+			if ( this._constructorException != null )
+			{
+				Assert.Fail( this._constructorException.ToString() );
+			}
+
+			this.TestDefaultConstructor();
+			this.TestMessageConstructor_WithMessage_SetToMessage();
+			this.TestMessageConstructor_WithNull_SetToDefaultMessage();
+			this.TestInnerExceptionConstructor_WithMessageAndInnerException_SetToMessageAndInnerException();
+			this.TestInnerExceptionConstructor_Null_SetToDefaultMessageAndNullInnerException();
+			this.TestSerialization();
+		}
+
+		private void TestDefaultConstructor()
+		{
+			var target = this._defaultConstructor();
+			Assert.That( target.Message, Is.Not.Null.And.Not.Empty, "Message should not be null nor empty." );
+			TestToString( target, typeof( T ).Name + "()" );
+		}
+
+		private void TestMessageConstructor_WithMessage_SetToMessage()
+		{
+			var message = Guid.NewGuid().ToString();
+			var target = this._messageConstructor( message );
+			Assert.That( target.Message, Is.Not.Null.And.StringContaining( message ), "Message should contain message argument." );
+			TestToString( target, typeof( T ).Name + "()" );
+		}
+
+		private void TestMessageConstructor_WithNull_SetToDefaultMessage()
+		{
+			var defaultInstance = this._defaultConstructor();
+			var target = this._messageConstructor( null );
+			Assert.That( target.Message, Is.Not.Null.And.StringContaining( defaultInstance.Message ), "Message should equal to default message." );
+			TestToString( target, typeof( T ).Name + "()" );
+		}
+
+		private void TestInnerExceptionConstructor_WithMessageAndInnerException_SetToMessageAndInnerException()
+		{
+			var message = Guid.NewGuid().ToString();
+			var innerException = new Exception();
+			var target = this._innerExceptionConstructor( message, innerException );
+			Assert.That( target.Message, Is.Not.Null.And.StringContaining( message ), "Message should contain message argument." );
+			Assert.That( target.InnerException, Is.SameAs( innerException ), "InnerException should contain innerException argument." );
+			TestToString( target, typeof( T ).Name + "()" );
+		}
+
+		private void TestInnerExceptionConstructor_Null_SetToDefaultMessageAndNullInnerException()
+		{
+			var defaultInstance = this._defaultConstructor();
+			var target = this._innerExceptionConstructor( null, null );
+			Assert.That( target.Message, Is.Not.Null.And.StringContaining( defaultInstance.Message ), "Message should equal to default message." );
+			Assert.That( target.InnerException, Is.Null, "InnerException should be null." );
+			TestToString( target, typeof( T ).Name + "()" );
+		}
+
+		private static void TestToString( T target, string ctor )
+		{
+			TestToStringCore( MakeStackTrace( target ), ctor );
+		}
+
+		private static T MakeStackTrace( T target )
+		{
+			try
+			{
+				throw target;
+			}
+			catch ( T ex )
+			{
+				return ex;
+			}
+		}
+
+		private static void TestToStringCore( T target, string ctor )
+		{
+			Assert.That( target.ToString(), Is.Not.Null.And.Not.Empty.And.StringContaining( target.Message ).And.StringContaining( target.GetType().FullName ).And.StringContaining( target.StackTrace ), "ToString() should contain Message, Type.FullName, and StackTrace ({0}).", ctor );
+			if ( target.InnerException != null )
+			{
+				Assert.That( target.ToString(), Is.Not.Null.And.StringContaining( target.InnerException.Message ), "ToString() should contain InnerException ({0}).", ctor );
+			}
+		}
+
+		private void TestSerialization()
+		{
+			Assert.That( typeof( T ), Is.BinarySerializable );
+
+			var innerMessage = Guid.NewGuid().ToString();
+			var message = Guid.NewGuid().ToString();
+			var target = this._innerExceptionConstructor( message, new Exception( innerMessage ) );
+			using ( var buffer = new MemoryStream() )
+			{
+				var serializer = new BinaryFormatter();
+				serializer.Serialize( buffer, target );
+				buffer.Position = 0;
+				var deserialized = serializer.Deserialize( buffer ) as T;
+				Assert.That( deserialized, Is.Not.Null );
+				Assert.That( deserialized.Message, Is.EqualTo( target.Message ) );
+				Assert.That( deserialized.InnerException, Is.Not.Null.And.TypeOf( typeof( Exception ) ) );
+				Assert.That( deserialized.InnerException.Message, Is.EqualTo( target.InnerException.Message ) );
+			}
+		}
+	}
+}
