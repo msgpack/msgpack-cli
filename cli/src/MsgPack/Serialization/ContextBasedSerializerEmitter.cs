0f0c93b376f17faa8a358a98b17cc7b42f04be4e
yfakariya <yfakariya@gmail.com>
2012-01-28 14:00:12 +0900

    Code cleanup.

---
+++ cli/src/MsgPack/Serialization/ContextBasedSerializerEmitter.cs
@@ -0,0 +1,104 @@
+﻿#region -- License Terms --
+//
+// MessagePack for CLI
+//
+// Copyright (C) 2010 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using NLiblet.Reflection;
+
+namespace MsgPack.Serialization
+{
+	/// <summary>
+	///		<see cref="SerializerEmitter"/> using <see cref="SerializationContext"/> to hold serializers for target members.
+	/// </summary>
+	internal sealed class ContextBasedSerializerEmitter : SerializerEmitter
+	{
+		private readonly Type _targetType;
+		private readonly DynamicMethod _packToMethod;
+		private readonly DynamicMethod _unpackFromMethod;
+		private DynamicMethod _unpackToMethod;
+
+		public ContextBasedSerializerEmitter( Type targetType )
+		{
+			this._targetType = targetType;
+
+			this._packToMethod = new DynamicMethod( "PackToCore", null, new[] { typeof( SerializationContext ), typeof( Packer ), targetType } );
+			this._unpackFromMethod = new DynamicMethod( "UnpackFromCore", targetType, new[] { typeof( SerializationContext ), typeof( Unpacker ) } );
+		}
+
+		public override TracingILGenerator GetPackToMethodILGenerator()
+		{
+			if ( IsTraceEnabled )
+			{
+				this.Trace.WriteLine( "{0}::{1}", MethodBase.GetCurrentMethod(), this._unpackFromMethod );
+			}
+
+			return new TracingILGenerator( this._packToMethod, this.Trace );
+		}
+
+		public override TracingILGenerator GetUnpackFromMethodILGenerator()
+		{
+			if ( IsTraceEnabled )
+			{
+				this.Trace.WriteLine( "{0}::{1}", MethodBase.GetCurrentMethod(), this._unpackFromMethod );
+			}
+
+			return new TracingILGenerator( this._unpackFromMethod, this.Trace );
+		}
+
+		public override TracingILGenerator GetUnpackToMethodILGenerator()
+		{
+			if ( this._unpackToMethod == null )
+			{
+				this._unpackToMethod = new DynamicMethod( "UnpackToCore", null, new[] { typeof( SerializationContext ), typeof( Unpacker ), this._targetType } );
+			}
+
+			if ( IsTraceEnabled )
+			{
+				this.Trace.WriteLine( "{0}::{1}", MethodBase.GetCurrentMethod(), this._unpackToMethod );
+			}
+
+			return new TracingILGenerator( this._unpackToMethod, this.Trace );
+		}
+
+		public override MessagePackSerializer<T> CreateInstance<T>( SerializationContext context )
+		{
+			var packTo = this._packToMethod.CreateDelegate( typeof( Action<SerializationContext, Packer, T> ) ) as Action<SerializationContext, Packer, T>;
+			var unpackFrom = this._unpackFromMethod.CreateDelegate( typeof( Func<SerializationContext, Unpacker, T> ) ) as Func<SerializationContext, Unpacker, T>;
+			var unpackTo = default( Action<SerializationContext, Unpacker, T> );
+			if ( this._unpackToMethod != null )
+			{
+				unpackTo = this._unpackToMethod.CreateDelegate( typeof( Action<SerializationContext, Unpacker, T> ) ) as Action<SerializationContext, Unpacker, T>;
+			}
+
+			return new CallbackMessagePackSerializer<T>( context, packTo, unpackFrom, unpackTo );
+		}
+
+		public override Action<TracingILGenerator, int> RegisterSerializer( Type targetType )
+		{
+			return
+				( il, contextIndex ) =>
+				{
+					il.EmitAnyLdarg( contextIndex );
+					il.EmitAnyCall( Metadata._SerializationContext.GetSerializer1_Method.MakeGenericMethod( targetType ) );
+				};
+		}
+	}
+}
---
+++ cli/src/MsgPack/Serialization/FieldBasedSerializerEmitter.cs
@@ -0,0 +1,218 @@
+﻿#region -- License Terms --
+//
+// MessagePack for CLI
+//
+// Copyright (C) 2010 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using NLiblet.Reflection;
+
+namespace MsgPack.Serialization
+{
+	/// <summary>
+	///		<see cref="SerializerEmitter"/> using instance fields to hold serializers for target members.
+	/// </summary>
+	internal sealed class FieldBasedSerializerEmitter : SerializerEmitter
+	{
+		private static readonly Type[] _constructorParameterTypes = new[] { typeof( SerializationContext ) };
+
+		private readonly Dictionary<RuntimeTypeHandle, FieldBuilder> _serializers;
+		private readonly ConstructorBuilder _constructorBuilder;
+		private readonly TypeBuilder _typeBuilder;
+		private readonly MethodBuilder _packMethodBuilder;
+		private readonly MethodBuilder _unpackFromMethodBuilder;
+		private MethodBuilder _unpackToMethodBuilder;
+		private readonly bool _isDebuggable;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FieldBasedSerializerEmitter"/> class.
+		/// </summary>
+		/// <param name="host">The host <see cref="ModuleBuilder"/>.</param>
+		/// <param name="sequence">The sequence number to name new type..</param>
+		/// <param name="targetType">Type of the serialization target.</param>
+		/// <param name="isDebuggable">Set to <c>true</c> when <paramref name="host"/> is debuggable.</param>
+		public FieldBasedSerializerEmitter( ModuleBuilder host, int sequence, Type targetType, bool isDebuggable )
+			: base()
+		{
+			string typeName =
+				String.Join(
+				Type.Delimiter.ToString(),
+				typeof( SerializerEmitter ).Namespace,
+				"Generated",
+				IdentifierUtility.EscapeTypeName( targetType ) + "Serializer" + sequence
+			);
+			Tracer.Emit.TraceEvent( Tracer.EventType.DefineType, Tracer.EventId.DefineType, "Create {0}", typeName );
+			this._typeBuilder =
+				host.DefineType(
+					typeName,
+					TypeAttributes.Sealed | TypeAttributes.Public | TypeAttributes.AutoClass | TypeAttributes.AutoLayout | TypeAttributes.BeforeFieldInit,
+					typeof( MessagePackSerializer<> ).MakeGenericType( targetType )
+				);
+
+			this._constructorBuilder = this._typeBuilder.DefineConstructor( MethodAttributes.Public, CallingConventions.Standard, _constructorParameterTypes );
+
+			this._packMethodBuilder =
+				this._typeBuilder.DefineMethod(
+					"PackToCore",
+					MethodAttributes.Family | MethodAttributes.Virtual | MethodAttributes.Final,
+					CallingConventions.HasThis,
+					typeof( void ),
+					new Type[] { typeof( Packer ), targetType }
+				);
+
+			this._unpackFromMethodBuilder =
+				this._typeBuilder.DefineMethod(
+					"UnpackFromCore",
+					MethodAttributes.Family | MethodAttributes.Virtual | MethodAttributes.Final,
+					CallingConventions.HasThis,
+					targetType,
+					UnpackFromCoreParameterTypes
+				);
+
+			this._typeBuilder.DefineMethodOverride( this._packMethodBuilder, this._typeBuilder.BaseType.GetMethod( this._packMethodBuilder.Name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic ) );
+			this._typeBuilder.DefineMethodOverride( this._unpackFromMethodBuilder, this._typeBuilder.BaseType.GetMethod( this._unpackFromMethodBuilder.Name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic ) );
+			this._serializers = new Dictionary<RuntimeTypeHandle, FieldBuilder>();
+			this._isDebuggable = isDebuggable;
+		}
+
+		public sealed override TracingILGenerator GetPackToMethodILGenerator()
+		{
+			if ( IsTraceEnabled )
+			{
+				this.Trace.WriteLine( "{0}::{1}", MethodBase.GetCurrentMethod(), this._packMethodBuilder );
+			}
+
+			return new TracingILGenerator( this._packMethodBuilder, this.Trace, this._isDebuggable );
+		}
+
+		public sealed override TracingILGenerator GetUnpackFromMethodILGenerator()
+		{
+			if ( IsTraceEnabled )
+			{
+				this.Trace.WriteLine( "{0}::{1}", MethodBase.GetCurrentMethod(), this._unpackFromMethodBuilder );
+			}
+
+			return new TracingILGenerator( this._unpackFromMethodBuilder, this.Trace, this._isDebuggable );
+		}
+
+		public sealed override TracingILGenerator GetUnpackToMethodILGenerator()
+		{
+			if ( IsTraceEnabled )
+			{
+				this.Trace.WriteLine( "{0}::{1}", MethodBase.GetCurrentMethod(), this._unpackToMethodBuilder );
+			}
+
+			if ( this._unpackToMethodBuilder == null )
+			{
+				this._unpackToMethodBuilder =
+					this._typeBuilder.DefineMethod(
+						"UnpackToCore",
+						MethodAttributes.Family | MethodAttributes.Virtual | MethodAttributes.Final,
+						CallingConventions.HasThis,
+						null,
+						new Type[] { typeof( Unpacker ), this._unpackFromMethodBuilder.ReturnType }
+					);
+
+				this._typeBuilder.DefineMethodOverride( this._unpackToMethodBuilder, this._typeBuilder.BaseType.GetMethod( this._unpackToMethodBuilder.Name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic ) );
+			}
+
+			return new TracingILGenerator( this._unpackToMethodBuilder, this.Trace, this._isDebuggable );
+		}
+
+		public sealed override MessagePackSerializer<T> CreateInstance<T>( SerializationContext context )
+		{
+			var contextParameter = Expression.Parameter( typeof( SerializationContext ) );
+			return
+				Expression.Lambda<Func<SerializationContext, MessagePackSerializer<T>>>(
+					Expression.New(
+						this.Create(),
+						contextParameter
+					),
+					contextParameter
+				).Compile()( context );
+		}
+
+
+		/// <summary>
+		///		Creates the serializer type built now and returns its constructor.
+		/// </summary>
+		/// <returns>
+		///		Newly built <see cref="MessagePackSerializer{T}"/> type constructor.
+		///		This value will not be <c>null</c>.
+		///	</returns>
+		private ConstructorInfo Create()
+		{
+			if ( !this._typeBuilder.IsCreated() )
+			{
+				/*
+				 *	.ctor( SerializationContext ) : base()
+				 *	{
+				 *		this._serializer0 = context.GetSerializer<T0>();
+				 *		this._serializer1 = context.GetSerializer<T1>();
+				 *		this._serializer2 = context.GetSerializer<T2>();
+				 *			:
+				 *	}
+				 */
+				var il = this._constructorBuilder.GetILGenerator();
+				// : base()
+				il.Emit( OpCodes.Ldarg_0 );
+				il.Emit( OpCodes.Call, this._typeBuilder.BaseType.GetConstructor( BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, Type.EmptyTypes, null ) );
+
+				// this._serializerN = context.GetSerializer<T>();
+				foreach ( var entry in this._serializers )
+				{
+					var targetType = Type.GetTypeFromHandle( entry.Key );
+					var getMethod = Metadata._SerializationContext.GetSerializer1_Method.MakeGenericMethod( targetType );
+					il.Emit( OpCodes.Ldarg_0 );
+					il.Emit( OpCodes.Ldarg_1 );
+					il.Emit( OpCodes.Callvirt, getMethod );
+					il.Emit( OpCodes.Stfld, entry.Value );
+				}
+
+				il.Emit( OpCodes.Ret );
+			}
+
+			return this._typeBuilder.CreateType().GetConstructor( _constructorParameterTypes );
+		}
+
+		public sealed override Action<TracingILGenerator, int> RegisterSerializer( Type targetType )
+		{
+			if ( this._typeBuilder.IsCreated() )
+			{
+				throw new InvalidOperationException( "Type is already built." );
+			}
+
+			FieldBuilder result;
+			if ( !this._serializers.TryGetValue( targetType.TypeHandle, out result ) )
+			{
+				result = this._typeBuilder.DefineField( "_serializer" + this._serializers.Count, typeof( MessagePackSerializer<> ).MakeGenericType( targetType ), FieldAttributes.Private | FieldAttributes.InitOnly );
+				this._serializers.Add( targetType.TypeHandle, result );
+			}
+
+			return
+				( il, thisIndex ) =>
+				{
+					il.EmitAnyLdarg( thisIndex );
+					il.EmitLdfld( result );
+				};
+		}
+	}
+}
