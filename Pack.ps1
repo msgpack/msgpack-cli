f99385ea8c556e8578e08e332c14c2042d63b908
yfakariya <yfakariya@gmail.com>
2012-05-27 22:05:39 +0900

    Support rebuild.

--- Pack.ps1
+++ Pack.ps1
@@ -1,18 +1,22 @@
+param([Switch]$Rebuild)
+
 [string]$temp = '.\nugettmp'
-[string]$builder
-[string]$sln
-if ( ![String]::IsNullOrEmpty( "$env:windir" ) )
+[string]$builder = "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
+[string]$sln = 'MsgPack.sln'
+
+[string]$nuspec = 'MsgPack.nuspec'
+
+$buildOptions = @()
+if( $Rebuild )
 {
-    $builder = "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-    $sln = ".\MsgPack.sln"
-}
-else
-{
-    $builder = 'xbuild'
-    $sln = "./MsgPack.mono.sln"
+    $buildOptions += '/t:Rebuild'
 }
 
-&$builder $sln /p:Configuration=Release
+$buildOptions += '/p:Configuration=Release'
+
+&$builder $sln $buildOptions
+
 [string]$source =  [IO.Path]::GetFullPath( '.\bin' )
 $excludes = @('*.pdb', '*.resources.dll', 'System.*', 'Microsoft.*', 'Mono.*')
 
@@ -21,5 +25,5 @@ ls $source -Recurse -Exclude $excludes | copy -Destination { Join-Path ".\$temp\
 # Remove empty directories to exclude extra Silverlight related resources...
 ls ".\$temp\lib" -Recurse | where { ( $_.Attributes -band [IO.FileAttributes]::Directory ) -and ( ( [IO.DirectoryInfo]$_ ).GetFiles().Length -eq 0 ) } | Remove-Item
 
-.\.nuget\nuget.exe pack MsgPack.nuspec -BasePath .\nugettmp
+.\.nuget\nuget.exe pack $nuspec -BasePath .\nugettmp
 Remove-Item $temp -Force -Recurse
