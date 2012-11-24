f04f8d069b8ed2ef691976fe03b1ab9dbb43f386
yfakariya <yfakariya@gmail.com>
2012-11-25 01:23:49 +0900

    Fix typos.

--- src/MsgPack/Serialization/SerializationExceptions.cs
+++ src/MsgPack/Serialization/SerializationExceptions.cs
@@ -28,7 +28,7 @@ using System.Runtime.Serialization;
 namespace MsgPack.Serialization
 {
 	/// <summary>
-	///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+	///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 	///		Defines common exception factory methods.
 	/// </summary>
 	[EditorBrowsable( EditorBrowsableState.Never )]
@@ -37,7 +37,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewValueTypeCannotBeNull3Method = FromExpression.ToMethod( ( string name, Type memberType, Type declaringType ) => SerializationExceptions.NewValueTypeCannotBeNull( name, memberType, declaringType ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that value type cannot be <c>null</c> on deserialization.
 		/// </summary>
 		/// <param name="name">The name of the member.</param>
@@ -55,7 +55,7 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that value type cannot be <c>null</c> on deserialization.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -74,7 +74,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewTypeCannotSerializeMethod = FromExpression.ToMethod( ( Type type ) => NewTypeCannotSerialize( type ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that value type cannot serialize.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -88,12 +88,12 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<see cref="MethodInfo"/> of <see cref="NewTypeCannotDeserialize"/> method.
+		///		<see cref="MethodInfo"/> of <see cref="NewTypeCannotDeserialize(Type)"/> method.
 		/// </summary>
 		internal static readonly MethodInfo NewTypeCannotDeserializeMethod = FromExpression.ToMethod( ( Type type ) => NewTypeCannotDeserialize( type ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that value type cannot deserialize.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -112,7 +112,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewMissingItemMethod = FromExpression.ToMethod( ( int index ) => NewMissingItem( index ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that item is not found on the unpacking stream.
 		/// </summary>
 		/// <param name="index">The index to be unpacking.</param>
@@ -126,7 +126,7 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that target type is not serializable because it does not have public default constructor.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -140,7 +140,7 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that target type is not serializable because it does not have both of public default constructor and public constructor with an Int32 parameter.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -154,7 +154,7 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that there are no serializable fields and properties on the target type.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -173,7 +173,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewMissingPropertyMethod = FromExpression.ToMethod( ( string name ) => NewMissingProperty( name ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that required field is not found on the unpacking stream.
 		/// </summary>
 		/// <param name="name">The name of the property.</param>
@@ -192,7 +192,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewUnexpectedEndOfStreamMethod = FromExpression.ToMethod( () => NewUnexpectedEndOfStream() );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that unpacking stream ends on unexpectedly position.
 		/// </summary>
 		/// <returns><see cref="Exception"/> instance. It will not be <c>null</c>.</returns>
@@ -204,7 +204,7 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that target collection type does not declare appropriate Add(T) method.
 		/// </summary>
 		/// <param name="type">The target type.</param>
@@ -220,7 +220,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewIsNotArrayHeaderMethod = FromExpression.ToMethod( () => SerializationExceptions.NewIsNotArrayHeader() );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that unpacker is not in the array header, that is the state is invalid.
 		/// </summary>
 		/// <returns><see cref="Exception"/> instance. It will not be <c>null</c>.</returns>
@@ -232,7 +232,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewIsNotMapHeaderMethod = FromExpression.ToMethod( () => SerializationExceptions.NewIsNotMapHeader() );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that unpacker is not in the array header, that is the state is invalid.
 		/// </summary>
 		/// <returns><see cref="Exception"/> instance. It will not be <c>null</c>.</returns>
@@ -246,7 +246,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewNotSupportedBecauseCannotInstanciateAbstractTypeMethod = FromExpression.ToMethod( ( Type type ) => SerializationExceptions.NewNotSupportedBecauseCannotInstanciateAbstractType( type ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that operation is not supported because <paramref name="type"/> cannot be instanciated.
 		/// </summary>
 		/// <param name="type">Type.</param>
@@ -262,7 +262,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewTupleCardinarityIsNotMatchMethod = FromExpression.ToMethod( ( int expected, int actual ) => SerializationExceptions.NewTupleCardinarityIsNotMatch( expected, actual ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the array length does not match to expected tuple cardinality.
 		/// </summary>
 		/// <param name="expectedTupleCardinality">The expected cardinality of the tuple.</param>
@@ -279,7 +279,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewIsIncorrectStreamMethod = FromExpression.ToMethod( ( Exception innerException ) => SerializationExceptions.NewIsIncorrectStream( innerException ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the underlying stream is not correct semantically because failed to unpack items count of array/map.
 		/// </summary>
 		/// <param name="innerException">The inner exception for the debug. The value is implementation specific.</param>
@@ -294,7 +294,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewIsTooLargeCollectionMethod = FromExpression.ToMethod( () => SerializationExceptions.NewIsTooLargeCollection() );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the unpacking collection is too large to represents in the current runtime environment.
 		/// </summary>
 		/// <returns><see cref="Exception"/> instance. It will not be <c>null</c>.</returns>
@@ -306,7 +306,7 @@ namespace MsgPack.Serialization
 		}
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the the unpacker does not contain any data because the underlying stream is empty or unpacker has not been started.
 		/// </summary>
 		/// <returns><see cref="Exception"/> instance. It will not be <c>null</c>.</returns>
@@ -320,7 +320,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewNullIsProhibitedMethod = FromExpression.ToMethod( ( string memberName ) => SerializationExceptions.NewNullIsProhibited( memberName ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the member cannot be <c>null</c> or the unpacking value cannot be nil because nil value is explicitly prohibitted.
 		/// </summary>
 		/// <param name="memberName">The name of the member.</param>
@@ -336,7 +336,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewReadOnlyMemberItemsMustNotBeNullMethod = FromExpression.ToMethod( ( string memberName ) => SerializationExceptions.NewReadOnlyMemberItemsMustNotBeNull( memberName ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the unpacking value cannot be nil because the target member is read only and its type is collection.
 		/// </summary>
 		/// <param name="memberName">The name of the member.</param>
@@ -352,7 +352,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewStreamDoesNotContainCollectionForMemberMethod = FromExpression.ToMethod( ( string memberName ) => SerializationExceptions.NewStreamDoesNotContainCollectionForMember( memberName ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the unpacking collection value is not a collection.
 		/// </summary>
 		/// <param name="memberName">The name of the member.</param>
@@ -368,7 +368,7 @@ namespace MsgPack.Serialization
 		internal static readonly MethodInfo NewUnexpectedArrayLengthMethod = FromExpression.ToMethod( ( int expectedLength, int actualLength ) => SerializationExceptions.NewUnexpectedArrayLength( expectedLength, actualLength ) );
 
 		/// <summary>
-		///		<strong>This is intened to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
+		///		<strong>This is intended to MsgPack for CLI internal use. Do not use this type from application directly.</strong>
 		///		Returns new exception to notify that the unpacking array size is not expected length.
 		/// </summary>
 		/// <param name="expectedLength">Expected, required for deserialization array length.</param>
