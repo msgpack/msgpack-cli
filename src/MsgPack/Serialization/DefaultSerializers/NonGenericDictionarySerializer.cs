ee82455506a09e554268af1039dd014291a3314b
yfakariya <yfakariya@gmail.com>
2014-05-25 02:34:55 +0900

    Fix non-generic dictionary serializer does not pack key value pair correctly.

--- src/MsgPack/Serialization/DefaultSerializers/NonGenericDictionarySerializer.cs
+++ src/MsgPack/Serialization/DefaultSerializers/NonGenericDictionarySerializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.Serialization;
 
 namespace MsgPack.Serialization.DefaultSerializers
 {
@@ -8,13 +9,11 @@ namespace MsgPack.Serialization.DefaultSerializers
 	/// </summary>
 	internal sealed class NonGenericDictionarySerializer : MessagePackSerializer<IDictionary>
 	{
-		private readonly System_Collections_DictionaryEntryMessagePackSerializer _entrySerializer;
 		private readonly IMessagePackSerializer _collectionDeserializer;
 
 		public NonGenericDictionarySerializer( SerializationContext ownerContext, Type targetType )
 			: base( ownerContext )
 		{
-			this._entrySerializer = new System_Collections_DictionaryEntryMessagePackSerializer( ownerContext );
 			this._collectionDeserializer = ownerContext.GetSerializer( targetType );
 		}
 
@@ -23,8 +22,19 @@ namespace MsgPack.Serialization.DefaultSerializers
 			packer.PackMapHeader( objectTree.Count );
 			foreach ( DictionaryEntry item in objectTree )
 			{
-				this._entrySerializer.PackToCore( packer, item );
-			}
+				if ( !( item.Key is MessagePackObject ) )
+				{
+					throw new SerializationException("Non generic dictionary may contain only MessagePackObject typed key.");
+				}
+
+				( item.Key as IPackable ).PackToMessage( packer, null );
+
+				if ( !( item.Value is MessagePackObject ) )
+				{
+					throw new SerializationException("Non generic dictionary may contain only MessagePackObject typed value.");
+				}
+
+				( item.Value as IPackable ).PackToMessage( packer, null );			}
 		}
 
 		protected internal override IDictionary UnpackFromCore( Unpacker unpacker )
