8fd194460d57e0e9f4987c9870be945ca40bd0ae
yfakariya <yfakariya@gmail.com>
2014-05-25 19:58:59 +0900

    Fix shell script.

--- PackUnity.sh
+++ PackUnity.sh
@@ -5,7 +5,7 @@
 
 UNITY=/Applications/Unity/Unity.app/Contents/MacOS/Unity 
 
-if [ -! f $UNITY ];
+if [ ! -f $UNITY ];
 then
     echo "Unity is not found."
     exit 1
@@ -13,15 +13,15 @@ fi
 
 MPU=./tools/mpu/bin/mpu.exe
 
-if [ -! f $MPU ];
+if [ ! -f $MPU ];
 then
-    xbuild ./src/mpu/mpu.csproj -p:Configuration=Release
-}
+    xbuild ./MsgPack.compats.sln /p:Configuration=Release
+fi
 
-PROJECTPATH=$(cd $(dirname "./src/MsgPack.Unity") && pwd)/$(basename "./src/MsgPack.Unity")
+PROJECTPATH=$(cd $(dirname "./src/MsgPack.Unity") && pwd)/$(basename "./src/MsgPack.Unity")/
 PACKAGENAME=$(cd $(dirname "./MsgPack.unitypackage") && pwd)/$(basename "./MsgPack.unitypackage")
 
-del -rf ./src/MsgPack.Unity/Assets/MsgPack/*
-del -f ./MsgPack.unitypackage
+rm -rdf ./src/MsgPack.Unity/Assets/MsgPack/*
+rm -f ./MsgPack.unitypackage
 mono $MPU -l -p ./src/MsgPack.Xamarin.iOS/MsgPack.Xamarin.iOS.csproj -o ./src/MsgPack.Unity/Assets/MsgPack/ -w
 $UNITY -quit -projectPath PROJECTPATH -exportPackage Assets/MsgPack PACKAGENAME
