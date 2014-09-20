5a099a7fdd55508b8f1c1b09cf8fb2beec630038
yfakariya <yfakariya@gmail.com>
2014-09-21 02:35:29 +0900

    Refine build scripts.

---
+++ Pack.bat
@@ -0,0 +1,1 @@
+@powershell .\pack.ps1
--- Pack.ps1
+++ Pack.ps1
@@ -24,8 +24,19 @@ $xamarinFile = New-Object IO.FileInfo( ".\bin\monotouch\MsgPack.dll" )
 if( ( $winFile.LastWriteTime - $xamarinFile.LastWriteTime ).Days -ne 0 )
 {
 	# It might that I forgot building in xamarin when winRT build and xamarin build last write time are differ more than 1day.
-	Write-Error "Last write times between WinRT binary and Xamarin library are very differ. Do you forget to place latest Xamarin build (on Mac) or latest WinRT build (on Windows) on ./bin ?"
-	return
+	#Write-Error "Last write times between WinRT binary and Xamarin library are very differ. Do you forget to place latest Xamarin build (on Mac) or latest WinRT build (on Windows) on ./bin ?"
+	#return
 }
 
 .\.nuget\nuget.exe pack $nuspec
+
+# Unity
+Remove-Item .\MsgPack-CLI -Recurse
+Copy-Item .\bin\ .\MsgPack-CLI\ -Recurse
+[Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" ) | Out-Null
+# 'latest' should be rewritten with semver manually.
+Remove-Item .\MsgPack.Cli.latest.zip
+[IO.Compression.ZipFile]::CreateFromDirectory( ".\MsgPack-CLI", ".\MsgPack.Cli.latest.zip" )
+Remove-Item .\MsgPack-CLI -Recurse
+
+Write-Host "Package creation finished. Ensure AssemblyInfo.cs is updated and .\SetFileVersions.ps1 was executed."
