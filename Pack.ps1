71261db73e652ec8bf391850be4d6a410176466f
yfakariya <yfakariya@gmail.com>
2014-09-21 18:55:12 +0900

    Fix script comment-out.

--- Pack.ps1
+++ Pack.ps1
@@ -24,8 +24,8 @@ $xamarinFile = New-Object IO.FileInfo( ".\bin\monotouch\MsgPack.dll" )
 if( ( $winFile.LastWriteTime - $xamarinFile.LastWriteTime ).Days -ne 0 )
 {
 	# It might that I forgot building in xamarin when winRT build and xamarin build last write time are differ more than 1day.
-	#Write-Error "Last write times between WinRT binary and Xamarin library are very differ. Do you forget to place latest Xamarin build (on Mac) or latest WinRT build (on Windows) on ./bin ?"
-	#return
+	Write-Error "Last write times between WinRT binary and Xamarin library are very differ. Do you forget to place latest Xamarin build (on Mac) or latest WinRT build (on Windows) on ./bin ?"
+	return
 }
 
 .\.nuget\nuget.exe pack $nuspec
