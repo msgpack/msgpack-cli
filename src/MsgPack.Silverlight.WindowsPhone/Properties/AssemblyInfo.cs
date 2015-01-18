21f9ecab7433f03cd47fec4d9b7f57f7f3de464b
yfakariya <yfakariya@gmail.com>
2015-01-18 16:42:02 +0900

    Update versions.

--- src/MsgPack.Silverlight.WindowsPhone/Properties/AssemblyInfo.cs
+++ src/MsgPack.Silverlight.WindowsPhone/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Security;
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2010-2014" )]
 
 
-[assembly: AssemblyFileVersion( "0.5.1817.707" )]
+[assembly: AssemblyFileVersion( "0.5.1838.683" )]
 
 [assembly: AllowPartiallyTrustedCallers]
 
