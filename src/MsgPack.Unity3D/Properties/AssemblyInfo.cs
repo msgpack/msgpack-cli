130e78cde17577abc5629f12ac54d7c21c12901d
yfakariya <yfakariya@gmail.com>
2015-02-11 11:30:13 +0900

    Update version info.

--- src/MsgPack.Unity3D/Properties/AssemblyInfo.cs
+++ src/MsgPack.Unity3D/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Security;
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2010-2014" )]
 
 
-[assembly: AssemblyFileVersion( "0.5.1857.601" )]
+[assembly: AssemblyFileVersion( "0.5.1867.150" )]
 
 [assembly: AllowPartiallyTrustedCallers]
--- src/mpu/Properties/AssemblyInfo.cs
+++ src/mpu/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2014" )]
 
-[assembly: AssemblyFileVersion( "0.5.1857.601" )]
+[assembly: AssemblyFileVersion( "0.5.1867.150" )]
 
 
