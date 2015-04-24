a2aa5b789c1b1313f5da463594ed42fac506299b
yfakariya <yfakariya@gmail.com>
2015-04-25 03:33:18 +0900

    Update hand-made serializer for testing.

--- test/MsgPack.UnitTest/Serialization/EchoKeyedCollection_2MessagePackSerializer`2.cs
+++ test/MsgPack.UnitTest/Serialization/EchoKeyedCollection_2MessagePackSerializer`2.cs
@@ -21,6 +21,8 @@
 using System;
 using System.Collections.ObjectModel;
 
+using MsgPack.Serialization.CollectionSerializers;
+
 namespace MsgPack.Serialization
 {
 	/// <summary>
@@ -29,66 +31,14 @@ namespace MsgPack.Serialization
 	/// <typeparam name="TKey">The type of keys of the <see cref="EchoKeyedCollection{TKey, TValue}"/>.</typeparam>
 	/// <typeparam name="T">The type of items of the <see cref="EchoKeyedCollection{TKey, TValue}"/>.</typeparam>
 	// ReSharper disable once InconsistentNaming
-	internal class EchoKeyedCollection_2MessagePackSerializer<TKey, T> : MessagePackSerializer<EchoKeyedCollection<TKey, T>>
+	internal class EchoKeyedCollection_2MessagePackSerializer<TKey, T> : CollectionMessagePackSerializer<EchoKeyedCollection<TKey, T>, T>
 	{
-		private readonly MessagePackSerializer<T> _itemSerializer;
-
 		public EchoKeyedCollection_2MessagePackSerializer( SerializationContext ownerContext, PolymorphismSchema itemsSchema )
-			: base( ownerContext )
+			: base( ownerContext, itemsSchema ) { }
+
+		protected override EchoKeyedCollection<TKey, T> CreateInstance( int initialCapacity )
 		{
-			this._itemSerializer = ownerContext.GetSerializer<T>( itemsSchema );
-		}
-
-		protected internal override void PackToCore( Packer packer, EchoKeyedCollection<TKey, T> objectTree )
-		{
-			PackerUnpackerExtensions.PackCollectionCore( packer, objectTree, this._itemSerializer );
-		}
-
-		[System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0", Justification = "Asserted internally" )]
-		protected internal override EchoKeyedCollection<TKey, T> UnpackFromCore( Unpacker unpacker )
-		{
-			if ( !unpacker.IsArrayHeader )
-			{
-				throw SerializationExceptions.NewIsNotArrayHeader();
-			}
-
-			var count = UnpackHelpers.GetItemsCount( unpacker );
-			var collection = new EchoKeyedCollection<TKey, T>();
-			this.UnpackToCore( unpacker, collection, count );
-			return collection;
-		}
-
-		[System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0", Justification = "Asserted internally" )]
-		protected internal override void UnpackToCore( Unpacker unpacker, EchoKeyedCollection<TKey, T> collection )
-		{
-			if ( !unpacker.IsArrayHeader )
-			{
-				throw SerializationExceptions.NewIsNotArrayHeader();
-			}
-
-			this.UnpackToCore( unpacker, collection, UnpackHelpers.GetItemsCount( unpacker ) );
-		}
-
-		private void UnpackToCore( Unpacker unpacker, KeyedCollection<TKey, T> collection, int count )
-		{
-			for ( int i = 0; i < count; i++ )
-			{
-				if ( !unpacker.Read() )
-				{
-					throw SerializationExceptions.NewUnexpectedEndOfStream();
-				}
-				if ( unpacker.IsCollectionHeader )
-				{
-					using ( var subTreeUnpacker = unpacker.ReadSubtree() )
-					{
-						collection.Add( this._itemSerializer.UnpackFromCore( subTreeUnpacker ) );
-					}
-				}
-				else
-				{
-					collection.Add( this._itemSerializer.UnpackFromCore( unpacker ) );
-				}
-			}
+			return new EchoKeyedCollection<TKey, T>();
 		}
 	}
 }
