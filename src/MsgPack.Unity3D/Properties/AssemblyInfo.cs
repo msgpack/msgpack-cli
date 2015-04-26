bd0cc4a7d80a0a82cd868e1a2daef38c2ab65165
yfakariya <yfakariya@gmail.com>
2015-04-27 00:26:22 +0900

    Update version info.

--- src/MsgPack.Unity3D/Properties/AssemblyInfo.cs
+++ src/MsgPack.Unity3D/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Security;
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2010-2014" )]
 
 
-[assembly: AssemblyFileVersion( "0.6.1886.867" )]
+[assembly: AssemblyFileVersion( "0.6.1941.926" )]
 
 [assembly: AllowPartiallyTrustedCallers]
--- src/MsgPack.Xamarin.Android/Properties/AssemblyInfo.cs
+++ src/MsgPack.Xamarin.Android/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Security;
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2010-2014" )]
 
 
-[assembly: AssemblyFileVersion( "0.6.1886.867" )]
+[assembly: AssemblyFileVersion( "0.6.1941.926" )]
 
 [assembly: SecurityRules( SecurityRuleSet.Level2, SkipVerificationInFullTrust = true )]
 [assembly: AllowPartiallyTrustedCallers]
