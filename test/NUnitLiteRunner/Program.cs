5cba15e6cc6fcc8ee0197ffbfbe052339dd08ea8
yfakariya <yfakariya@gmail.com>
2015-05-23 22:41:16 +0900

    Code clean up.

--- test/NUnitLiteRunner/Program.cs
+++ test/NUnitLiteRunner/Program.cs
@@ -20,10 +20,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 
 using MsgPack.Serialization;
 
