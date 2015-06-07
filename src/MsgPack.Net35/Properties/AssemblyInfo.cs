55160bff52444bd8ab9fea411b1579257781eb2b
yfakariya <yfakariya@gmail.com>
2015-06-07 16:57:34 +0900

    Update version info.

--- src/MsgPack.Net35/Properties/AssemblyInfo.cs
+++ src/MsgPack.Net35/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Security;
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2010-2014" )]
 
 
-[assembly: AssemblyFileVersion( "0.6.1969.613" )]
+[assembly: AssemblyFileVersion( "0.6.1990.830" )]
 
 [assembly: AllowPartiallyTrustedCallers]
 
--- src/mpu/Properties/AssemblyInfo.cs
+++ src/mpu/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2014" )]
 
-[assembly: AssemblyFileVersion( "0.6.1969.613" )]
+[assembly: AssemblyFileVersion( "0.6.1990.830" )]
 
 
