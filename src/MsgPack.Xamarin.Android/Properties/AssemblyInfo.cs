d9557c11333ea61d827a8b1cf83521c4e7c3a9aa
yfakariya <yfakariya@gmail.com>
2015-07-05 22:36:35 +0900

    Update version info.

--- src/MsgPack.Xamarin.Android/Properties/AssemblyInfo.cs
+++ src/MsgPack.Xamarin.Android/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Security;
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2010-2014" )]
 
 
-[assembly: AssemblyFileVersion( "0.6.1990.830" )]
+[assembly: AssemblyFileVersion( "0.6.2011.810" )]
 
 [assembly: SecurityRules( SecurityRuleSet.Level2, SkipVerificationInFullTrust = true )]
 [assembly: AllowPartiallyTrustedCallers]
