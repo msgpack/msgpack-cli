5fe59387be09808f0bb2180628dfed141c87ce19
yfakariya <yfakariya@gmail.com>
2015-08-09 16:22:18 +0900

    Update versions.

--- src/mpu/Properties/AssemblyInfo.cs
+++ src/mpu/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCopyright( "Copyright © FUJIWARA, Yusuke 2014" )]
 
-[assembly: AssemblyFileVersion( "0.6.2026.267" )]
+[assembly: AssemblyFileVersion( "0.6.2046.442" )]
 
 
