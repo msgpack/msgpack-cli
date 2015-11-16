3d82094c794e44caf79105673355b9581b72faf9
yfakariya <yfakariya@gmail.com>
2015-11-17 00:52:40 +0900

    Remove wrong copy&pasted copyrights from file headers.

--- src/MsgPack/Serialization/AbstractSerializers/SerializerBuilder`3.Nullable.cs
+++ src/MsgPack/Serialization/AbstractSerializers/SerializerBuilder`3.Nullable.cs
@@ -16,9 +16,6 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 //
-// Contributors:
-//    Takeshi KIRIYA
-//
 #endregion -- License Terms --
 
 using System;
