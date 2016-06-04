91199705b1f238590809bd69537e73668635c253
yfakariya <yfakariya@gmail.com>
2016-06-04 20:06:50 +0900

    Fix build script.

--- build/SetBuildEnv.ps1
+++ build/SetBuildEnv.ps1
@@ -1,5 +1,3 @@
 # Set versions for AssemblyInfo.cs
 $env:PackageVersion = ( Get-Content .\Version.txt );
 $env:AssemblyBaseVersion = $env:PackageVersion | foreach{ if( $_ -match "^\d+\.\d+" ){ $matches[0] } } 
-# Update Android SDK
-Echo 'y' | & "$env:ANDROID_HOME/tools/android.bat" update sdk --no-ui --all --filter android-10,android-23,platform-tools,tools,build-tools-23.0.3
