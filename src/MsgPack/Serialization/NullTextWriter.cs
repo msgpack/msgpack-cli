98f0485e81f0578a3852099e614ba12e70b75f4c
yfakariya <yfakariya@gmail.com>
2016-07-09 23:51:44 +0900

    Fix Silverlight and .NET Core build for PR #179.
    This commit includes:
    * Synchronize projects.
    * Add conditionals for Silverlight, UWP, and .NET Standards.

--- src/MsgPack/Serialization/NullTextWriter.cs
+++ src/MsgPack/Serialization/NullTextWriter.cs
@@ -19,42 +19,56 @@ namespace MsgPack.Serialization
 			get { return Encoding.UTF8; }
 		}
 
-		public override void Close() { }
+		public override void Write( string value ) { }
 		public override void Flush() { }
-		public override void Write(char value) { }
-		public override void Write(char[] buffer) { }
-		public override void Write(char[] buffer, int index, int count) { }
-		public override void Write(bool value) { }
-		public override void Write(int value) { }
-		public override void Write(uint value) { }
-		public override void Write(long value) { }
-		public override void Write(ulong value) { }
-		public override void Write(float value) { }
-		public override void Write(double value) { }
-		public override void Write(decimal value) { }
-		public override void Write(string value) { }
-		public override void Write(object value) { }
-		public override void Write(string format, object arg0) { }
-		public override void Write(string format, object arg0, object arg1) { }
-		public override void Write(string format, object arg0, object arg1, object arg2) { }
-		public override void Write(string format, object[] arg) { }
+#if !NETFX_CORE && !NETSTANDARD1_1 && !NETSTANDARD1_3
+		public override void Close() { }
+#endif // !NETFX_CORE && !NETSTANDARD1_1 && !NETSTANDARD1_3
+		public override void Write( char value ) { }
+		public override void Write( char[] buffer ) { }
+		public override void Write( char[] buffer, int index, int count ) { }
+		public override void Write( bool value ) { }
+		public override void Write( int value ) { }
+		public override void Write( uint value ) { }
+		public override void Write( long value ) { }
+		public override void Write( ulong value ) { }
+		public override void Write( float value ) { }
+		public override void Write( double value ) { }
+		public override void Write( decimal value ) { }
+		public override void Write( object value ) { }
+#if !NETSTANDARD1_1
+		public override void Write( string format, object arg0 ) { }
+#endif // !NETSTANDARD1_1
+#if !NETSTANDARD1_1
+		public override void Write( string format, object arg0, object arg1 ) { }
+#endif // !NETSTANDARD1_1
+#if !SILVERLIGHT && !NETSTANDARD1_1
+		public override void Write( string format, object arg0, object arg1, object arg2 ) { }
+#endif // !SILVERLIGHT && !NETSTANDARD1_1
+		public override void Write( string format, object[] arg ) { }
 		public override void WriteLine() { }
-		public override void WriteLine(char value) { }
-		public override void WriteLine(char[] buffer) { }
-		public override void WriteLine(char[] buffer, int index, int count) { }
-		public override void WriteLine(bool value) { }
-		public override void WriteLine(int value) { }
-		public override void WriteLine(uint value) { }
-		public override void WriteLine(long value) { }
-		public override void WriteLine(ulong value) { }
-		public override void WriteLine(float value) { }
-		public override void WriteLine(double value) { }
-		public override void WriteLine(decimal value) { }
-		public override void WriteLine(string value) { }
-		public override void WriteLine(object value) { }
-		public override void WriteLine(string format, object arg0) { }
-		public override void WriteLine(string format, object arg0, object arg1) { }
-		public override void WriteLine(string format, object arg0, object arg1, object arg2) { }
-		public override void WriteLine(string format, object[] arg) { }
+		public override void WriteLine( char value ) { }
+		public override void WriteLine( char[] buffer ) { }
+		public override void WriteLine( char[] buffer, int index, int count ) { }
+		public override void WriteLine( bool value ) { }
+		public override void WriteLine( int value ) { }
+		public override void WriteLine( uint value ) { }
+		public override void WriteLine( long value ) { }
+		public override void WriteLine( ulong value ) { }
+		public override void WriteLine( float value ) { }
+		public override void WriteLine( double value ) { }
+		public override void WriteLine( decimal value ) { }
+		public override void WriteLine( string value ) { }
+		public override void WriteLine( object value ) { }
+#if !NETSTANDARD1_1
+		public override void WriteLine( string format, object arg0 ) { }
+#endif // !NETSTANDARD1_1
+#if !NETSTANDARD1_1
+		public override void WriteLine( string format, object arg0, object arg1 ) { }
+#endif // !NETSTANDARD1_1
+#if !SILVERLIGHT && !NETSTANDARD1_1
+		public override void WriteLine( string format, object arg0, object arg1, object arg2 ) { }
+#endif // !SILVERLIGHT && !NETSTANDARD1_1
+		public override void WriteLine( string format, object[] arg ) { }
 	}
 }
