3f3d48006db612b5506c7583c47697e5e73d5f41
yfakariya <yfakariya@gmail.com>
2016-09-24 16:56:17 +0900

    Fix iOS build.
    This commit removes Xamarin iOS classic project and migrate unit test to unified project.

--- test/MsgPack.UnitTest.Xamarin.iOS/Main.cs
+++ test/MsgPack.UnitTest.Xamarin.iOS/Main.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 
-using MsgPack.Serialization;
+using UIKit;
 
-namespace MsgPack.UnitTest.Xamios
+namespace MsgPack
 {
 	public class Application
 	{
 		// This is the main entry point of the application.
 		static void Main( string[] args )
 		{
-			// if you want to use a different Application Delegate class from "UnitTestAppDelegate"
+			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-			UIApplication.Main( args, null, "UnitTestAppDelegate" );
+			UIApplication.Main( args, null, "AppDelegate" );
 		}
 	}
 }
