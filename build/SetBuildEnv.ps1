96f5dc6e89e8717fcdcb82edc3b8173681c8f45c
yfakariya <yfakariya@gmail.com>
2016-09-24 09:22:38 +0900

    Fix version suffix and logs.

--- build/SetBuildEnv.ps1
+++ build/SetBuildEnv.ps1
@@ -7,15 +7,15 @@ if ( $env:APPVEYOR_REPO_TAG -ne "True" )
 	{
 		$now = [DateTime]::UtcNow
 		$daysSpan = $now - ( New-Object DateTime( $now.Year, 1, 1 ) )
-		$env:PackageVersion = "${version}-dev-{0:yy}{1:000}" -f @( $now, $daysSpan.Days )
+		$env:PackageVersion = "${version}-{0:yy}{1:000}" -f @( $now, $daysSpan.Days )
 	}
 	elseif ( ${version} -match "^[\d.]+$" )
 	{
-		$env:PackageVersion = "${version}-final-${env:APPVEYOR_BUILD_NUMBER}"
+		$env:PackageVersion = "${version}-${env:APPVEYOR_BUILD_NUMBER}"
 	}
 	else
 	{
-		$env:PackageVersion = "${version}-dev-${env:APPVEYOR_BUILD_NUMBER}"
+		$env:PackageVersion = "${version}-${env:APPVEYOR_BUILD_NUMBER}"
 	}
 }
 else
@@ -23,4 +23,4 @@ else
 	$env:PackageVersion = $version
 }
 
-Write-Information "version:'${version}', AssemblyBaseVersion:'${AssemblyBaseVersion}', PackageVersion:'${PackageVersion}'"
+Write-Host "version:'${version}', AssemblyBaseVersion:'${AssemblyBaseVersion}', PackageVersion:'${PackageVersion}'"
