4bc69bc46562fa291ece5c1f78acd020d69d5059
yfakariya <yfakariya@gmail.com>
2016-09-25 23:35:10 +0900

    Add lightweight TargetInvocationException free activation.
    This commit enabled removing (relatively heavy) System.Linq.Expressions dependency.

--- src/MsgPack/Serialization/ReflectionExtensions.cs
+++ src/MsgPack/Serialization/ReflectionExtensions.cs
@@ -37,6 +37,9 @@ using System.Diagnostics.Contracts;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
+#if !AOT && !NETSTANDARD1_1 && !NETSTANDARD1_3
+using System.Reflection.Emit;
+#endif // !AOT && !NETSTANDARD1_1 && !NETSTANDARD1_3
 
 using MsgPack.Serialization.Reflection;
 
@@ -145,6 +148,135 @@ namespace MsgPack.Serialization
 				return null;
 			}
 		}
+
+#warning TODO: Split multiple files
+#warning TODO: Remove System.Linq.Expressions
+		public static Type[] GetParameterTypes( this MethodBase source )
+		{
+			var parameters = source.GetParameters();
+			Type[] parameterTypes = new Type[ parameters.Length ];
+			for ( var i = 0; i < parameters.Length; i++ )
+			{
+				parameterTypes[ i ] = parameters[ i ].ParameterType;
+			}
+
+			return parameterTypes;
+		}
+
+#if !AOT && !NETSTANDARD1_1 && !NETSTANDARD1_3
+		public static Func<T> CreateConstructorDelegate<T>()
+		{
+			return CreateConstructorDelegate<T>( typeof( T ) );
+		}
+
+		public static Func<T> CreateConstructorDelegate<T>( this Type source )
+		{
+			ValidateInstanceType( source, typeof( T ) );
+
+			ConstructorInfo defaultConstructor = null;
+
+			if ( !typeof( T ).GetIsValueType() )
+			{
+				defaultConstructor = GetConstructor( typeof( T ), ReflectionAbstractions.EmptyTypes );
+
+				if ( defaultConstructor == null )
+				{
+					throw new InvalidOperationException(
+						String.Format(
+							CultureInfo.CurrentCulture,
+							"There are no default constructors in type '{0}'.",
+							typeof( T )
+						)
+					);
+				}
+			}
+
+			return ( Func<T> )CreateDelegate( typeof( Func<T> ), typeof( T ), defaultConstructor, ReflectionAbstractions.EmptyTypes );
+		}
+
+		public static Func<TArg, T> CreateConstructorDelegate<T, TArg>()
+		{
+			return CreateConstructorDelegate<T, TArg>( typeof( T ) );
+		}
+
+		public static Func<TArg, T> CreateConstructorDelegate<T, TArg>( this Type source )
+		{
+			ValidateInstanceType( source, typeof( T ) );
+			var parameterTypes = new[] { typeof( TArg ) };
+			return ( Func<TArg, T> )CreateDelegate( typeof( Func<TArg, T> ), typeof( T ), GetConstructor( typeof( T ), parameterTypes ), parameterTypes );
+		}
+
+		private static void ValidateInstanceType( Type source, Type returnType )
+		{
+			if ( source == null )
+			{
+				throw new ArgumentNullException( "source" );
+			}
+
+			if ( !returnType.IsAssignableFrom( source ) )
+			{
+				throw new InvalidOperationException(
+					String.Format(
+						CultureInfo.CurrentCulture,
+						"The instance type '{0}' is not assignable to return type '{1}'.",
+						source,
+						returnType
+					)
+				);
+			}
+
+		}
+
+		private static ConstructorInfo GetConstructor( Type source, Type[] parameterTypes )
+		{
+			var constructor = source.GetRuntimeConstructor( parameterTypes );
+			if ( constructor != null )
+			{
+				return constructor;
+			}
+
+			throw new InvalidOperationException(
+				String.Format(
+					CultureInfo.CurrentCulture,
+					"There are no constructors in type '{0}' which matches parameter types '{1}'.",
+					source,
+					String.Join( ", ", parameterTypes.Select( t => t.FullName ).ToArray() )
+				)
+			);
+		}
+
+		private static Delegate CreateDelegate( Type delegateType, Type targetType, ConstructorInfo constructor, Type[] parameterTypes )
+		{
+			var dynamicMethod =
+#if !SILVERLIGHT
+				new DynamicMethod( "Create" + targetType.Name, targetType, parameterTypes, restrictedSkipVisibility: true );
+#else
+				new DynamicMethod( "Create" + targetType.Name, targetType, parameterTypes );
+#endif // !SILVERLIGHT
+
+			var il = new TracingILGenerator( dynamicMethod, NullTextWriter.Instance, isDebuggable: false );
+			if ( constructor == null )
+			{
+				// Value type's init.
+				il.DeclareLocal( targetType );
+				il.EmitAnyLdloca( 0 );
+				il.EmitInitobj( targetType );
+				il.EmitAnyLdloc( 0 );
+			}
+			else
+			{
+				for ( var i = 0; i < parameterTypes.Length; i++ )
+				{
+					il.EmitAnyLdarg( i );
+				}
+
+				il.EmitNewobj( constructor );
+			}
+
+			il.EmitRet();
+			return dynamicMethod.CreateDelegate( delegateType );
+		}
+#endif // !AOT && !NETSTANDARD1_1 && !NETSTANDARD1_3
 
 		public static Type GetMemberValueType( this MemberInfo source )
 		{
