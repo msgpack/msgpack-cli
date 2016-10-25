7e85956bbb8c4b9e929610afc7d17fdd381b634a
yfakariya <yfakariya@gmail.com>
2016-10-25 12:14:56 +0900

    Remove SerializerCodeGenerationConfiguration.ProviderName
    This property is not used because Roslyn based provider was gone.

--- src/MsgPack/Serialization/SerializerCodeGenerationConfiguration.cs
+++ src/MsgPack/Serialization/SerializerCodeGenerationConfiguration.cs
@@ -236,20 +236,6 @@ namespace MsgPack.Serialization
 		/// </value>
 		public bool WithAsync { get; set; }
 
-		private string _providerName;
-
-#warning TODO: mpu
-		/// <summary>
-		///		Gets or sets a code provider name.
-		/// </summary>
-		/// <value>
-		///		A code provider name. Setting <c>null</c> causes setting empty string which represents default provider.
-		/// </value>
-		public string ProviderName
-		{
-			get { return this._providerName; }
-			set { this._providerName = value ?? String.Empty; }
-		}
 
 #warning TODO: mpu
 		/// <summary>
