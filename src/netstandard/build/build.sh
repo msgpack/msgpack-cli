f72f06d69cc38fd95efe3495191af9500f213c3b
yfakariya <yfakariya@gmail.com>
2017-03-26 23:48:22 +0900

    Migrate Windows specific projects
    ...but it is not vs2017 because of silverlights.

--- src/netstandard/build/build.sh
+++ src/netstandard/build/build.sh
@@ -2,7 +2,7 @@
 echo Retore netstandard 1.1 project...
 echo Solution=$1
 echo Configuration=$2
-dotnet restore $1src/MsgPack.CoreClr/project.json
+dotnet restore $1src/MsgPack/MsgPack.csproj
 if test $? -ne 0; then
   echo "Failed to restore netstandard1.1/1.3"
   return 1 1>/dev/null 2>&1
@@ -10,7 +10,7 @@ if test $? -ne 0; then
 fi
 
 echo Build netstandard 1.1/1.3 project...
-dotnet build $1src/MsgPack.CoreClr/project.json -c $2
+dotnet build $1src/MsgPack/MsgPack.csproj -c $2
 if test $? -ne 0; then
   echo "Failed to build netstandard1.1/1.3"
   return 1 1>/dev/null 2>&1
