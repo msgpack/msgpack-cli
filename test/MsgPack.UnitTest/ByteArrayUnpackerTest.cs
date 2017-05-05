83781637efcfb1ccaefebff2376c01b8dd5eb68c
yfakariya <yfakariya@gmail.com>
2017-05-06 01:27:58 +0900

    Add byte array unpacker tests.

---
+++ test/MsgPack.UnitTest/ByteArrayUnpackerTest.cs
@@ -0,0 +1,47 @@
+﻿#region -- License Terms --
+//
+// MessagePack for CLI
+//
+// Copyright (C) 2017 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System.IO;
+#if !MSTEST
+#else
+using TestFixtureAttribute = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.TestClassAttribute;
+using TestAttribute = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.TestMethodAttribute;
+using TimeoutAttribute = NUnit.Framework.TimeoutAttribute;
+using Assert = NUnit.Framework.Assert;
+using Is = NUnit.Framework.Is;
+#endif
+
+namespace MsgPack
+{
+	public abstract partial class ByteArrayUnpackerTest : UnpackerTest
+	{
+		protected override bool ShouldCheckStreamPosition
+		{
+			get { return false; }
+		}
+
+		protected sealed override Unpacker CreateUnpacker( MemoryStream stream )
+		{
+			return this.CreateUnpacker( stream.ToArray() );
+		}
+
+		protected abstract Unpacker CreateUnpacker( byte[] source );
+	}
+}
