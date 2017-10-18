b30e1c9b44f5b927c359721ce7ae4940aca4167a
yfakariya <yfakariya@gmail.com>
2017-10-19 01:39:21 +0900

    Stabilize tests

--- test/MsgPack.UnitTest/CollectionValidatingByteArrayUnpackerTest.cs
+++ test/MsgPack.UnitTest/CollectionValidatingByteArrayUnpackerTest.cs
@@ -1,4 +1,4 @@
-﻿#region -- License Terms --
+#region -- License Terms --
 //
 // MessagePack for CLI
 //
@@ -33,7 +33,7 @@ using Is = NUnit.Framework.Is;
 namespace MsgPack
 {
 	[TestFixture]
-	[Timeout( 3000 )]
+	[Timeout( 30000 )]
 	public class CollectionValidatingByteArrayUnpackerTest : ByteArrayUnpackerTest
 	{
 		protected override bool ShouldCheckSubtreeUnpacker
