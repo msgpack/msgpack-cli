7c1caea9d3ce5368d14642323ae2630d8b479008
yfakariya <yfakariya@gmail.com>
2018-03-25 22:17:29 +0900

    Fix copyright headers.

--- src/MsgPack/TimestampStringConverter.ToString.cs
+++ src/MsgPack/TimestampStringConverter.ToString.cs
@@ -2,7 +2,7 @@
 //
 // MessagePack for CLI
 //
-// Copyright (C) 2010-2015 FUJIWARA, Yusuke
+// Copyright (C) 2017 FUJIWARA, Yusuke
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
