eb2cfd48d9de7c9c49786ac953fcab9bedc6b070
yfakariya <yfakariya@gmail.com>
2018-05-01 13:40:40 +0900

    Add test running scripts for Unity.

---
+++ test/MsgPack.UnitTest.Unity.Il2cpp.Full/run-ios-macos.sh
@@ -0,0 +1,1 @@
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -testPlatform ios -projectPath `pwd` -nographics -testResults `pwd`/result.xml
---
+++ test/MsgPack.UnitTest.Unity.Il2cpp.Full/run-win.cmd
@@ -0,0 +1,1 @@
+Unity.exe -runTests -batchmode -testPlatform StandaloneWindows64 -projectPath "%CD%" -nographics -testResults "%CD%/result.xml"
