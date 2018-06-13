de309da9348b7b512f301b595b434a41e2aaa828
yfakariya <yfakariya@gmail.com>
2018-06-13 23:15:37 +0900

    Separate .NET 3.5 CodeDOM test from other .NET 3.5 test to avoid AppVeyor timeout.

---
+++ build/RunUnitTests35-CodeDOM.cmd
@@ -0,0 +1,2 @@
+nunit3-console %APPVEYOR_BUILD_FOLDER%/test/MsgPack.UnitTest.CodeDom.Net35/bin/Debug/net35/MsgPack.UnitTest.CodeDom.Net35.dll --framework:net-3.5 --result=test-result-net35-codedom.xml;format=AppVeyor
+if not %errorlevel% == 0 exit /b 1
--- build/RunUnitTests35.cmd
+++ build/RunUnitTests35.cmd
@@ -1,4 +1,2 @@
 nunit3-console %APPVEYOR_BUILD_FOLDER%/test/MsgPack.UnitTest.Net35/bin/Debug/net35/MsgPack.UnitTest.Net35.dll --framework:net-3.5 --result=test-result-net35.xml;format=AppVeyor
 if not %errorlevel% == 0 exit /b 1
-nunit3-console %APPVEYOR_BUILD_FOLDER%/test/MsgPack.UnitTest.CodeDom.Net35/bin/Debug/net35/MsgPack.UnitTest.CodeDom.Net35.dll --framework:net-3.5 --result=test-result-net35-codedom.xml;format=AppVeyor
-if not %errorlevel% == 0 exit /b 1
