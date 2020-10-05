18d2d5922065988bcbb8ed9f2a778db0537a3400
yfakariya <yfakariya@gmail.com>
2020-10-05 19:15:31 +0900

    Revert "TODO REBASE Migrate CommonAssemblyInfo to props"
    This reverts commit 4e91318dbcef8263fca7972a885f68cde4217e9e.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+// Copyright (c) FUJIWARA, Yusuke and all contributors.
+// This file is licensed under Apache2 license.
+// See the LICENSE in the project root for more information.
+
+// Universal informations for entire MsgPack for CLI.
+
+using System;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+
