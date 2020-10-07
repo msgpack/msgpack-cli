7f64323f15fdab0273047ce1ceb4741b697c6e89
yfakariya <yfakariya@gmail.com>
2020-10-07 21:19:42 +0900

    Add default encoding

---
+++ src/MsgPack.Abstraction/Internal/Utf8EncodingNonBomStrict.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) FUJIWARA, Yusuke and all contributors.
+// This file is licensed under Apache2 license.
+// See the LICENSE in the project root for more information.
+
+using System.Text;
+
+namespace MsgPack.Internal
+{
+	/// <summary>
+	///		Defines sealed UTF-8 class which cannot emit BOM and throws Exception.
+	/// </summary>
+	/// <remarks>
+	///		Sealed class typed static constant is important for JIT devirtualization optimization.
+	/// </remarks>
+	internal sealed class Utf8EncodingNonBomStrict : UTF8Encoding
+	{
+		public static Utf8EncodingNonBomStrict Instance { get; } = new Utf8EncodingNonBomStrict();
+
+		private Utf8EncodingNonBomStrict() : base(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true)
+		{
+			this.DecoderFallback = DecoderFallback.ExceptionFallback;
+		}
+	}
+}
